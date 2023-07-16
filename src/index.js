f9a399db6dea66be9e5cc012622d25d456819844
tsaniawarda2 <63621268+tsaniawarda2@users.noreply.github.com>
2023-07-17 03:40:29 +0700

    setup

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const routes = require("./routes");
+const errorHandler = require("./middleware/errorHandler");
+
+const port = process.env.APP_PORT;
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use("/v1", routes);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`${process.env.APP_NAME} is running on ${port}`);
+});
---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,15 @@
+module.exports = (error, req, res, next) => {
+  if (error.code && error.message) {
+    return res.status(error.code).json({
+      error: error.message,
+    });
+    // Menangani kesalahan validasi Sequelize
+  } else if (error.name === "SequelizeValidationError") {
+    const errors = error.errors.map((error) => error.message);
+    res.status(400).json({ errors });
+  } else {
+    return res.status(500).json({
+      error: error.message || "Internal Server Error",
+    });
+  }
+};
