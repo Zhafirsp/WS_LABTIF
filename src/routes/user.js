2f9326b740f56006875726beb604666815bebe52
tsaniawarda2 <63621268+tsaniawarda2@users.noreply.github.com>
2023-08-05 18:17:54 +0700

    update user route

--- src/routes/user.js
+++ src/routes/user.js
@@ -7,7 +7,7 @@ const Authenticated = require("../middleware/authentication");
 
 router.post("/mahasiswa", UserController.addMahasiswaAsUser);
 router.post("/register", UserController.addUser);
-router.get("/", UserController.getAllUser);
+router.get("/", Authenticated, UserController.getAllUser);
 router.get("/:id", UserController.getUserById);
 router.put("/:id", UserController.updateUserById);
 router.delete("/:id", UserController.deleteUserById);
